c2842d197ab7de51267eaf064b4fe5cb11875c21
Antonio <aizzo1983@gmail.com>
2023-09-18 08:52:26 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+
+</head>
+<body>
+<h2> Hola Mundo.</h2>
+<h2> Hola como estan?.</h2>
+<h2> Primer cambio.</h2>
+
+
+</body>
+</html>
